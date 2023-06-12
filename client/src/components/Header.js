80d1d4e726298a24b4e07e0dd5b71e9a2ebedd47
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-06-12 14:36:59 -0400

    created structure and functionality for mobile nav with collapsing sub nav.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,20 @@
 import Nav from "./Nav";
 import MobNav from "./NavMobile";
+import { useMediaQuery } from 'react-responsive'
 
 
 function Header ( ){
+
+    const isDesktop = useMediaQuery({query: '(min-width: 992px)'})
+
+    console.log(isDesktop)
+
     return(
         <div id="header-container">
             <img style={{width: 100}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949" alt="placeholder" />
-            <Nav />
-            <MobNav />
+            
+            {isDesktop ? <Nav /> : <MobNav />} 
+            
         </div>
     )
 }
--- client/src/components/NavMobile.js
+++ client/src/components/NavMobile.js
@@ -1,10 +1,39 @@
-
+import {Link} from "react-router-dom";
+import { useState } from "react";
 
 
 function MobNav () {
+
+    const [displayShowSub, setDisplayShowSub] = useState(false);
+    const [displayOppSub, setDisplayOppSub] = useState(false);
+    
+    const handleDisplayShow = () => {
+        setDisplayShowSub(!displayShowSub);
+    }
+    const handleDisplayOpp = () => {
+        setDisplayOppSub(!displayOppSub);
+    }
+
+
     return(
         <div id="mob-nav-container">
-            <h1>Mobile Nav</h1>
+            <Link to="about">About</Link>
+            <h3 onClick={handleDisplayShow}>Shows +</h3>
+                {displayShowSub ? 
+                    <div className="sub-nav">  
+                        <Link to="upcoming"> —Upcoming Shows</Link>
+                        <Link to="past"> —Past Shows</Link>  
+                    </div> 
+                : null }
+            <h3 onClick={handleDisplayOpp}>Opportunities +</h3>
+                {displayOppSub ? 
+                    <div className="sub-nav">   
+                        <Link to="booking"> —Book Us</Link>
+                        <Link to="sponsorship"> —Sponsorship</Link>
+                        <Link to="submissions"> —Submissions</Link>
+                    </div> 
+                    : null }
+            <Link to="contact">Contact</Link>
         </div>
     )
 }
